763eeb5f3aeb31dc13c795151a93d28cf3ff599b
Zhehai <zhehaizhang1@gmail.com>
2021-09-18 03:22:06 -0400

    Assets

---
+++ Buildr/Resources/img/weapon/download.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+wget http://moomoo.io/img/weapons/${1}.png
+wget http://moomoo.io/img/weapons/${1}_g.png
+wget http://moomoo.io/img/weapons/${1}_d.png
+wget http://moomoo.io/img/weapons/${1}_r.png
