88943d1bc99f29fe311ad383fc50e4eb07a331ab
Nithin Muthukumar <33675303+nithinmuthukumar@users.noreply.github.com>
2021-09-18 10:43:40 -0400

    setting up player

--- Buildr/Source/Game/GameBoard.cpp
+++ Buildr/Source/Game/GameBoard.cpp
@@ -5,8 +5,13 @@
 
 using namespace Game;
 
-GameBoard::GameBoard()
+GameBoard::GameBoard():m_player(nullptr)
 {
+    m_player = new PlayerEntity();
+    GameEngine::GameEngineMain::GetInstance()->AddEntity(m_player);
+    m_player->SetPos(sf::Vector2f(50.f, 50.f));
+    m_player->SetSize(sf::Vector2f(40.f, 40.f));
+
 
 }
 
